b8311a83d27730b972ab843bf99ffe8e53e67497
nahom37 <aregayen@gmail.com>
2021-08-27 14:56:14 +0300

    sancho

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
-
-int main (void)
-
+/**
+  * main - Prints a text
+  *
+  * Return: Always 1 (Success)
+  */
+int main(void)
 {
-
-printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-
-return (1);
-
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\
+", 59);
+        return (1);
 }
