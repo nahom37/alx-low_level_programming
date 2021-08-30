f9a5e75aebccc56c8ee6aa32caba388f55d372d8
nahom37 <aregayen@gmail.com>
2021-08-30 23:24:38 +0300

    sancho

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,38 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
-  * main - Prints 3 combination of numbers
-  *
-  * Return: Always (Success)
-  */
+ * main - Writes combinations of 00-99
+ * @void: Empty parameter list for main.
+ *
+ * Description: Writes all unique combinations of 2
+ * digit numbers
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
-	int c, i, k, j;
+	int i, j;
 
-	for (c = 48; c <= 57; c++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (j = 0; j <= 99; j++)
 		{
-			for (k = 48; k <= 57; k++)
+			if (i < j)
 			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 
-					if (c + i + k + j == 227 && c == 57)
-					{
-					break;
-					}
-					else
-					{
+				if (i != 98 || j != 99)
+				{
 					putchar(',');
 					putchar(' ');
-					}
-					}
 				}
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
