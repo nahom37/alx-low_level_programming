8eecd6430bd7a41a84b62644693a2edf6ecb70d6
redieight <redietalx@gmail.com>
2021-09-10 21:27:15 +0300

    sancho

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,32 +1,54 @@
 #include "main.h"
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * Return: void
  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
-void print_line(char *c, int s, int l)
 {
-	int j, k;
+/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	for (j = 0; j <= 9; j++)
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - '0';
 		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-	for (k = 0; k <= s; k++)
+	if (d == 1)
 	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
+	return (r);
 }
