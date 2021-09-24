f07277dd9b7f64dcc92c8477fa1fad57093f2c1e
redieight <radietalx@gmail.com>
2021-09-24 09:06:52 +0300

    sancho

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,89 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * _isdigit - this function says if a character is a digit
+ * @n: a pointer to a string.
+ * a blank line
+ * Description: this function says if a character is a digit)?
+ * section header: the header of this function is holberton.h)*
+ * Return: return 1 if c is a digit and 0 in other cases.
  */
-int is_digit(char *s)
+int _isdigit(char *n)
 {
-int i = 0;
+  int i;
 
-while (s[i])
-{
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
+  i = 0;
+  while (*(n + i) != '\0')
+    {
+      if (*(n + i) < '0' || *(n + i) > '9')
+	return (0);
+      i++;
+    }
+  return (1);
 }
-return (1);
-}
-
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
+ * _strlen  - this functions prints the lenght of a string
+ * a blank line
+ *@s: string to print.
+ * Description: this function prints the lenght of a string?
+ * section header: the header of this function is holberton.h)*
+ * Return: retunrn the lenght in int
  */
 int _strlen(char *s)
 {
-int i = 0;
-
-while (s[i] != '\0')
-{
-i++;
+  if (*s != '\0')
+    {
+      s++;
+      return (1 + _strlen(s));
+    }
+  else
+    return (0);
 }
-return (i);
-}
-
 /**
- * errors - handles errors for main
- */
-void errors(void)
-{
-printf("Error\n");
-exit(98);
-}
-
-/**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * main - the entry point
+ * a blank line
+ *@argc:the number of argumentes
+ *@argv: the arguments to be multiplicated
+ * Description: this function multiply two numbers)?
+ * section header: the header of this function is holberton.h)*
+ * Return: return 0 in success.
  */
 int main(int argc, char *argv[])
 {
-char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+  int p, res, len, n1, n2, i, j;
+  int *total;
 
-s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-errors();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = malloc(sizeof(int) * len);
-if (!result)
-return (1);
-for (i = 0; i <= len1 + len2; i++)
-result[i] = 0;
-for (len1 = len1 - 1; len1 >= 0; len1--)
-{
-digit1 = s1[len1] - '0';
-carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-{
-digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
-}
-if (carry > 0)
-result[len1 + len2 + 1] += carry;
-}
-for (i = 0; i < len - 1; i++)
-{
-if (result[i])
-a = 1;
-if (a)
-_putchar(result[i] + '0');
-}
-if (!a)
-_putchar('0');
-_putchar('\n');
-free(result);
-return (0);
-}
+  if (argc < 3 || argc > 3 || !(_isdigit(argv[1])) || !(_isdigit(argv[2])))
+    puts("Error"), exit(98);
+  if (argv[1][0] == '0' || argv[2][0] == '0')
+    {
+      printf("0\n");
+      return (0);
+    }
+  n1 = _strlen(argv[1]), n2 = _strlen(argv[2]);
+  len = n1 + n2;
+  total = calloc(len, sizeof(int *));
+  if (total == NULL)
+    puts("Error"), exit(98);
+  for (i = (n2 - 1); i > -1; i--)
+    {
+      res = 0;
+      for (j = (n1 - 1); j > -1; j--)
+	{
+	  p = (argv[2][i] - '0') * (argv[1][j] - '0');
+	  res =  (p / 10);
+	  total[(i + j) + 1] += (p % 10);
+	  if (total[(i + j) + 1] > 9)
+	    {
+	      total[i + j] += total[(i + j) + 1] / 10;
+	      total[(i + j) + 1] = total[(i + j) + 1] % 10;
+	    }
+	  total[(i + j)] += res;
+	}
+    }
+  if (total[0] == 0)
+    i = 1;
+  else
+    i = 0;
+  for (; i < len; i++)
+    printf("%d", total[i]);
+  printf("\n");
