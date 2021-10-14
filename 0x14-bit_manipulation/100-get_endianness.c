50403b118b07adc400545f8fb838b4aac93bf75a
redieight <radietalx@gmail.com>
2021-10-14 19:27:50 +0300

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,8 +8,11 @@
 int get_endianness(void)
 
 {
-unsigned int i = 1;
-char *c = (char *) &i;
+	unsigned int i;
 
-return (*c);
+	i = 1;
+	if (*(char *)&i == 1)
+		return (1);
+	else
+		return (0);
 }
